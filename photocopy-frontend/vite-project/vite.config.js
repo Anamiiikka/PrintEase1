cf025488e06e1fd6445c176d091cfa5659a6ff69
verma20 <riyaverma3731@gmail.com>
2025-01-06 23:27:36 +0530

    remove base

--- photocopy-frontend/vite-project/vite.config.js
+++ photocopy-frontend/vite-project/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./", // Ensures relative paths in the output
   plugins: [react()],
   server: {
     proxy: {
