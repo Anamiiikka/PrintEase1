99506796811390c259cdcf3588a05d63e19c4ea4
verma20 <riyaverma3731@gmail.com>
2025-01-06 23:20:51 +0530

    base addition

--- photocopy-frontend/vite-project/vite.config.js
+++ photocopy-frontend/vite-project/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./", // Ensures relative paths in the output
   plugins: [react()],
   server: {
     proxy: {
