a4a12fe13d15bdc592e33ef16e6d26e166a2856f
Anamika <anamikasingh200205@gmail.com>
2025-01-06 21:41:08 +0530

    Deploy

--- photocopy-frontend/vite-project/vite.config.js
+++ photocopy-frontend/vite-project/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     proxy: {
       '/api': {
        
-        target: 'https://printease-o6ll.onrender.com/api/v1/users', // Your backend URL
+        target: 'https://printease-o6ll.onrender.com', // Your backend URL
         changeOrigin: true,  // Ensures the request is proxied correctly
         secure: false,       // Set to true if you're using HTTPS
       },
