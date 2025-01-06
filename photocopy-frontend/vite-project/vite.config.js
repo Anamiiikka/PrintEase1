8c979e8334fabc44469f11478920557ca21a569d
verma20 <riyaverma3731@gmail.com>
2025-01-06 22:48:49 +0530

    backend url

--- photocopy-frontend/vite-project/vite.config.js
+++ photocopy-frontend/vite-project/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     proxy: {
       '/api': {
        
-        target: 'https://printease-o6ll.onrender.com', // Your backend URL
+        target: 'https://printease1-99dh.onrender.com', // Your backend URL
         changeOrigin: true,  // Ensures the request is proxied correctly
         secure: false,       // Set to true if you're using HTTPS
       },
