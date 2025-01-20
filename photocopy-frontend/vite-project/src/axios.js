285228043a177e1f30d2aca5f1e8eb58c52c1646
verma-riya20 <riyaverma3731@gmail.com>
2025-01-20 01:54:32 -0800

    axios.js removed

--- photocopy-frontend/vite-project/src/axios.js
+++
@@ -1,7 +0,0 @@
-// src/axios.js
-import axios from 'axios';
-
-axios.defaults.baseURL = 'https://printease1-zcdn.onrender.com'; // Your backend URL
-axios.defaults.withCredentials = true; // Ensures cookies are sent with requests
-
-export default axios;
